1e37a8b8f260d0136b15230d3d119a6d93020380
Arpit Khatri <arpitkhatri5@gmail.com>
2020-11-26 01:55:45 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,25 +1,22 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
-    can_bounce = BooleanProperty(True)
 
     def bounce_ball(self, ball):
-        if self.collide_widget(ball) and self.can_bounce:
+        if self.collide_widget(ball):
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
-            self.can_bounce = False
-        elif not self.collide_widget(ball) and not self.can_bounce:
-            self.can_bounce = True
+
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
